603d529b9daa58be14e90b5385120ce423169709
heejiyang <dianoheeji@naver.com>
2022-11-22 17:54:19 +0900

    add: redux

---
+++ Redux/004/my-app/src/App.jsx
@@ -0,0 +1,11 @@
+import GoodsCounter from './Components/GoodsCounter'
+
+function App() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <GoodsCounter />
+    </div>
+  );
+}
+export default App;
